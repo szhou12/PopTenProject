1235529a170310b8f96ecd47ff96b7669f06ff74
ptprojectad <36282591+ptprojectad@users.noreply.github.com>
2018-03-08 23:39:19 -0600

    Update main_algorithm.py

--- main_algorithm.py
+++ main_algorithm.py
@@ -4,11 +4,13 @@ import analysis2
 
 TWEETS_PATH = './twitter_data3.txt'
 
-def get_format(rank, twi_df):
+def get_format(rank, twi_df, string_tuple):
     rank = rank[:10]
     output = {}
     l = len(rank)
-
+    
+    output['String1'] = string_tuple[0]
+    output['String2'] = string_tuple[1]
 
     for i in range(l):
         output['Restaurant'+str(i+1)] = rank[i][0]
@@ -43,6 +45,10 @@ def main_algorithm(data):
 
     raw_dict = raw_tuple[0]
     raw_list = raw_tuple[1]
+    next_best = raw_tuple[2]
+    exception_string = raw_tuple[3]
+    string_tuple = (next_best, exception_string)
+    
     res_dict = analysis2.dict_converter(raw_dict, raw_list, city)
 
     if not res_dict:
@@ -55,7 +61,7 @@ def main_algorithm(data):
             return (False, context)
         else:
             rank = analysis2.analyze_df(df)
-            context = get_format(rank, df)
+            context = get_format(rank, df, string_tuple)
             
             print('output: ', context)
             return (True, context)
