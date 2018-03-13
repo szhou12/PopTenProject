3e0447fedfdb975c03dac00aab1e4e09fd37f9f5
szhou12 <szhou12@uchicago.edu>
2018-03-13 02:17:18 -0500

    remove Archive

--- Archive/pptn/searchres/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SearchresConfig(AppConfig):
-    name = 'searchres'
--- Archive/pptn/searchres/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class cuisineForm(forms.Form):
-    food_type = forms.CharField(label='Food Type', max_length=100, widget=forms.TextInput(attrs={'placeholder':'burgers, pizza...'}))
-    city = forms.CharField(label='City', max_length=100)
-    address = forms.CharField(label='Address', max_length=100)
